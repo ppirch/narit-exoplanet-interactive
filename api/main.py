ac96bf889c5190eaef484bdae91e85686a86a399
ppirch <pakin.pirch@gmail.com>
2023-04-16 03:23:39 +0700

    fix: missing import CORSMiddleware

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from routers import batman_plot, exoplanet_plot, exoplanets
 
 origins = [
