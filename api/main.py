6dbef98bca9bd8e066033e9965cb4f79d5495779
ppirch <pakin.pirch@gmail.com>
2023-04-16 03:21:25 +0700

    feat: add cors

--- api/main.py
+++ api/main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import batman_plot, exoplanet_plot, exoplanets
 
+origins = [
+    "http://0.0.0.0:8888",
+    "http://localhost:8888"
+]
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(batman_plot.router)
 app.include_router(exoplanet_plot.router)
 app.include_router(exoplanets.router)
